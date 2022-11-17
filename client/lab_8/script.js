46b89b8ec03fc59a7ab0a706862ab7de3e2fd8dc
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2022-11-17 15:45:04 -0500

    finished code

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -98,20 +98,18 @@ function initMap() {
 }
 
 function markerPlace(array, map) {
-  console.log('markerPlace', array);
-  const marker = L.marker([51.5, -0.09]).addTo(map);
-  array.forEach(item => {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+  array.forEach((item, index)=> {
     const {coordinates} = item.geocoded_column_1;
     L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if(index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10)
+    }
   });
-
-  const circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
 }
 
 async function mainEvent() {
@@ -165,7 +163,7 @@ async function mainEvent() {
       console.log(event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
-      markerPlace(currentList, pageMap);
+      markerPlace(filteredList, pageMap);
     });
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
